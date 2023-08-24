c1daacd2a06d77723ab66e4e7835dbd38a340836
dendrita <dendritadev@gmail.com>
2023-08-24 14:04:46 +0200

    Assets Dinero

--- Assets/Greg/_Scripts/UI/CoinsText.cs
+++ Assets/Greg/_Scripts/UI/CoinsText.cs
@@ -14,6 +14,6 @@ public class CoinsText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = string.Format("Coins: {0}", dataContainer.coins.ToString());  
+        text.text = string.Format("{0}", dataContainer.coins.ToString());  
     }
 }
