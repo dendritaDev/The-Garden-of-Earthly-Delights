b60d5387b94b3d64b2d73acddc7d046ff4652d46
dendrita <dendritadev@gmail.com>
2023-09-01 12:44:52 +0200

    InGame Main Menu Panel

---
+++ Assets/Greg/_Scripts/UI/InGame UI/DOTween/FillCircleButtonsMainMenuTweening.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class FillCircleButtonsMainMenuTweening : UIBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField] private float rate = 1.25f;
+    [SerializeField] private float time = 0.25f;
+    [SerializeField] private Ease ease;
+    
+    private Vector3 BaseScale;
+
+    public AudioClip hover;
+    public AudioClip pressed;
+
+    public Image circleImage;
+    public RectTransform textTransform;
+    public TextMeshProUGUI text;
+
+    private AudioSource audioSource;
+
+    protected override void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    protected override void Start()
+    {
+        BaseScale = textTransform.transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        textTransform.transform.DOScale(BaseScale * rate, time)
+            .SetEase(ease);
+
+        text.color = new Color(255, 255, 255, 255);
+
+        circleImage.DOFillAmount(1, time * 5).SetEase(ease);
+
+        audioSource.PlayOneShot(hover);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        textTransform.transform.DOScale(BaseScale, time).SetEase(ease);
+
+        text.color = new Color(255, 255, 255, 255);
+
+        circleImage.DOFillAmount(0, time * 5).SetEase(ease);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(pressed);
+    }
+
+
+}
