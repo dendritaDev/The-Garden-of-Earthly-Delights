1839cb49501c2b06d3aefcd2f93511c3d11042a1
dendrita <dendritadev@gmail.com>
2023-09-07 14:37:01 +0200

    Upgrade DEscription Panel Tweening

--- Assets/Greg/_Scripts/UI/InGame UI/DOTween/UpgradeDescriptionPanelTweening.cs
+++ Assets/Greg/_Scripts/UI/InGame UI/DOTween/UpgradeDescriptionPanelTweening.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class UpgradeDescriptionPanelTweening : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private RectTransform FrameRectTransform;
+    [SerializeField] private Ease RectTransformEase;
+    [SerializeField] private float transformTime = 0.15f;
+    [SerializeField] private RectTransform TitleTextTransform;
+    [SerializeField] private Ease TitleTextEase;
+    [SerializeField] private float titleTextTime = 0.15f;
+    [SerializeField] private TextMeshProUGUI DescriptionText;
+    [SerializeField] private Ease DescritionTextEase;
+    [SerializeField] private float descriptionTextTime = 1f;
+
+    private DOTweenTMPAnimator TitleTextAnimator;
+    private void OnEnable()
     {
-        
+        Initialize();
+
+        TitleTextAnimator = new DOTweenTMPAnimator(DescriptionText);
+
+        var sequence = DOTween.Sequence()
+            .Append(FrameRectTransform.DOPunchScale(new Vector3(0.2f, 0.2f, 0.2f), transformTime)).SetEase(RectTransformEase)
+            .Append(TitleTextTransform.DOPunchScale(new Vector3(0.2f, 0.2f, 0.2f), titleTextTime)).SetEase(TitleTextEase).OnComplete(() =>
+            {
+                var sequence = DOTween.Sequence();
+                float time = descriptionTextTime / (float)TitleTextAnimator.textInfo.characterCount;
+                float constTime = time;
+                Debug.Log(time);
+                for (int i = 0; i < TitleTextAnimator.textInfo.characterCount; i++)
+                {
+                    sequence
+                        .Append(TitleTextAnimator.DOFadeChar(i, 1, time)).SetEase(DescritionTextEase)
+                        .Join(TitleTextAnimator.DOPunchCharScale(i, 1.5f, time)).SetEase(DescritionTextEase);
+
+                    time -= (constTime / (float)TitleTextAnimator.textInfo.characterCount);
+                }
+            });
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Initialize()
     {
         
+        DescriptionText.DOFade(0f, 0f);
     }
 }
